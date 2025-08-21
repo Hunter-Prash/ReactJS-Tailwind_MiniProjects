c9d9020a22e27361f6f136e096023d3b6477fa4b
Prashant Rajaji <103595753+Hunter-Prash@users.noreply.github.com>
2025-08-21 13:46:23 +0530

    Add routing and form components for multi-page form functionality

--- Multi Page Form Data (Storage Practise)/src/App.jsx
+++ Multi Page Form Data (Storage Practise)/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Address from './Address';
+import Pref from './Pref';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/address" element={<Address />} />
+        <Route path="/preferences" element={<Pref />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
